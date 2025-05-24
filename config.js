66d185f1a12f2f1940f823e0b896f7c9af76dea1
Sartor02 <alessandrosartore02@gmail.com>
2025-05-24 17:51:18 +0200

    improved agent speed

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    // host: "http://localhost:8080",
+    host: "http://localhost:8080",
     // host: "https://deliveroojs.onrender.com",
-    host: "https://deliveroojs25.azurewebsites.net/",
+    // host: "https://deliveroojs25.azurewebsites.net/",
 
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI1M2VhYiIsIm5hbWUiOiJ0ZXN0Iiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NDc4MzI5ODB9.8qscraV88TjTrG5BdK5DwElmgrDO8Kw2tTLkbLxgc_s'
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE1OGJkMCIsIm5hbWUiOiJ0ZXN0MSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ4MDY5MzQwfQ.7lFpwou_kHUZmzBBdz-6dsP62M4yd0e2AI9I6GQ8LYM'
 }
 
 export default config;
