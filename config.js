79385dd0ff8226dd03baf26a004625eeafbc0ec6
Sartor02 <alessandrosartore02@gmail.com>
2025-06-01 20:02:54 +0200

    config file corrected

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
-    host: "http://localhost:8080",
-    // host: "https://deliveroojs.onrender.com",
+    // host: "http://localhost:8080",
+    host: "https://deliveroojs.rtibdi.disi.unitn.it/",
     // host: "https://deliveroojs25.azurewebsites.net/",
 
-    token: ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNWZhNCIsIm5hbWUiOiJ0ZXN0MSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ3ODk0MzYzfQ.pKP_4DIcTW_JgxT7R4KDrD3WZ06BfrOWUaRy2qn3STI',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI3NmM5MiIsIm5hbWUiOiJ0ZXN0MiIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzQ3ODk0NTc3fQ.n-URL9lF-QA4Qwvwn_FRxTGkkyhPjDiP8xyCYCo66lo']
+    token: ['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjA4ODUzYSIsIm5hbWUiOiJEZXNjYW50YTEiLCJ0ZWFtSWQiOiJlZTFjYjkiLCJ0ZWFtTmFtZSI6IkRlc2NhbnRhQmF1Y2hpIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NDg0MTgzODF9.n893HOopARJKXEzePo6JBurQuYrBdf_heNK8gqZI5vI',
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImUxZDkyYSIsIm5hbWUiOiJEZXNjYW50YTIiLCJ0ZWFtSWQiOiIwYTI1ODMiLCJ0ZWFtTmFtZSI6IkRlc2NhbnRhQmF1Y2hpIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NDg0MTgzODR9.h3e65HXFYfTksqvLR8KUVMOjziGbWiZgVEFsuGx52MQ']
 }
 
 export default config;
